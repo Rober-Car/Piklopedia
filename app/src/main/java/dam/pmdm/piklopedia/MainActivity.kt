b460194a63eac427fd04e17320303f647571b2e3
Rober_Car <pollinox@hotmail.com>
2025-10-29 13:45:27 +0100

    aplicando stilos

--- app/src/main/java/dam/pmdm/piklopedia/MainActivity.kt
+++ app/src/main/java/dam/pmdm/piklopedia/MainActivity.kt
@@ -2,8 +2,7 @@ package dam.pmdm.piklopedia
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -83,5 +82,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         reciclador.adapter = adaptador
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
     }
 }
